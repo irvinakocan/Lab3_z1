2b88763d4c156a348d9ea9e4b7f1aa8b75bcc514
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-07-10 20:22:55 +0200

    Implementiran zadatak sa lab1 koristenjem  lambda funkcija i pomocnih funkcija za kolekcije podataka

--- Lab3_z1/Program.cs
+++ Lab3_z1/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Lab3_z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaRijeci = new List<string>();
+
+            Console.WriteLine("Unesite spisak riječi (razdvojenih razmakom): ");
+            string unos = Console.ReadLine();
+            string[] pojedinacneRijeci = unos.Split(" ");
+
+            foreach (var rijec in pojedinacneRijeci)
+                listaRijeci.Add(rijec);
+
+
+            // uklanjanje dupliranih rijeci
+            listaRijeci.RemoveAll(rijec1 =>
+                listaRijeci.Where(rijec2 => rijec1 == rijec2).ToList().Count > 1);
+
+            // popis svih unesenih riječi bez ispisivanja duplikata
+            foreach (var rijec in listaRijeci)
+                Console.WriteLine(rijec);
+
+
+        }
+    }
+}
